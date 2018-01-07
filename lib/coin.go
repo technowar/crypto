12077831935e89b526e21a1b9d9ca7967eac4a71
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2018-01-07 13:16:51 +0800

    Fix usage

--- lib/coin.go
+++ lib/coin.go
@@ -12,12 +12,12 @@ func Crypto(crypto string, coinList Coins, now time.Time) {
 	for _, item := range coinList.Coin {
 		if item.Symbol == crypto || strings.ToUpper(item.Id) == crypto {
 			detail = append(detail, item)
-		} else {
-			fmt.Printf("Unable to find %v in the database.\n\n", crypto)
-
-			return
 		}
 	}
 
-	Display(Coins{detail}, now)
+	if len(detail) == 0 {
+		fmt.Printf("Unable to find %v in the database.\n\n", crypto)
+	} else {
+		Display(Coins{detail}, now)
+	}
 }
--- utils/usage.go
+++ utils/usage.go
@@ -4,8 +4,9 @@ import "fmt"
 
 func Usage() {
 	fmt.Println("Commands available:")
-	fmt.Println("  help    \tDisplay help.")
-	fmt.Println("  watch   \tWatch top 10 coins.")
-	fmt.Println("  coin    \tDisplay specific coin. Defaults to BTC.")
+	fmt.Println("  help   Display help.")
+	fmt.Println("  watch  Watch top 10 coins.")
+	fmt.Println("  coin")
+	fmt.Println("    [<symbol>|<name>]  Display details of specific coin. Defaults to Bitcoin.")
 	fmt.Println("\nAll data are from https://coinmarketcap.com/api/")
 }
